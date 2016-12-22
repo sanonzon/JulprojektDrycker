2d130c4bf8e35ab03f57e1ac8af84b3b0281b632
Axel <pennsater@gmail.com>
2016-12-22 17:15:21 +0100

    saker funkar

--- JulprojektDrycker/Program.cs
+++ JulprojektDrycker/Program.cs
@@ -6,27 +6,13 @@ using System.Threading.Tasks;
 
 namespace JulprojektDrycker
 {
-    class Form
-    {
-        public static void PrintForm(string category)
-        {
-            Console.WriteLine("Generating form for '{0}'", category);
-            Console.WriteLine("");
-            Console.WriteLine("");
-            Console.WriteLine("");
-        }
-        
-    }
-
-
-    // TODO Kommentera bättre
     class DbMiddleware
     {
         public static List<Beer> beerList = new List<Beer>();
         public static List<Wine> wineList = new List<Wine>();
         public static List<Whisky> WhiskyList = new List<Whisky>();
 
-        public string category;
+        public static string category;
         private Dictionary<int, string> categoryDict = new Dictionary<int, string>()
         {
             { 1, "beer"}, {2,"wine" }, { 3, "whisky"}
@@ -37,13 +23,15 @@ namespace JulprojektDrycker
         {
             if (beerList.Count > 0)
             {
-                foreach(Beer b in beerList)
+                Console.WriteLine("--- Beer Inputs ---");
+                foreach (Beer b in beerList)
                 {
-                    Console.WriteLine("{0}, {1}, {2}\n{3}",b.Name,b.Type,b.AlcProcent,b.Description);
+                    Console.WriteLine("{0}, {1}, {2}\n{3}", b.Name, b.Type, b.AlcProcent, b.Description);
                 }
             }
             if (wineList.Count > 0)
             {
+                Console.WriteLine("--- Wine Inputs ---");
                 foreach (Wine b in wineList)
                 {
                     Console.WriteLine("{0}, {1}, {2}\n{3}", b.Name, b.Type, b.AlcProcent, b.Description);
@@ -51,6 +39,7 @@ namespace JulprojektDrycker
             }
             if (WhiskyList.Count > 0)
             {
+                Console.WriteLine("--- Whisky Inputs ---");
                 foreach (Whisky b in WhiskyList)
                 {
                     Console.WriteLine("{0}, {1}, {2}\n{3}", b.Name, b.Type, b.AlcProcent, b.Description);
@@ -69,11 +58,17 @@ namespace JulprojektDrycker
                 if (categoryDict.ContainsKey(key))
                 {
                     category = categoryDict[key];
-                    return true;                    
+                    Form.PrintForm(category);
+                    return true;
                 }
                 else if (key == 9)
                 {
                     PrintLists();
+                    return true;
+                }
+                else if (key == 0)
+                {
+                    return true;
                 }
             }
             return false;
@@ -81,26 +76,92 @@ namespace JulprojektDrycker
 
 
     }
+    class Form
+    {
+        // If User is bad and writes string instead of double or int. Sets value to 0.0...
+        public static double validateProcent(string s)
+        {
+            double d;
+            if (s.Contains(","))
+            {
+                s.Replace(",", ".");
+            }
+            if(double.TryParse(s, out d))
+            {
+                return d;
+            }
+            else
+            {
+                return 0.0;
+            }
+
+        }
+        public static void PrintForm(string category)
+        {
+            Console.WriteLine("Generating form for '{0}'.", category);
+            Console.Write("\nName: ");
+            string name = Console.ReadLine();
+            Console.Write("\nType: ");
+            string type = Console.ReadLine();
+            Console.Write("\n%: ");
+            string p = Console.ReadLine();
+            Console.Write("\nDescription: ");
+            string desc = Console.ReadLine();
+            Console.WriteLine();
+
+            double d = validateProcent(p);
+
+            switch (category)
+            {
+                case "beer":
+                    DbMiddleware.beerList.Add(new Beer(name,type,d,desc));
+                    break;
+                case "wine":
+                    DbMiddleware.wineList.Add(new Wine(name, type, d, desc));
+                    break;
+                case "whisky":
+                    DbMiddleware.WhiskyList.Add(new Whisky(name, type, d, desc));
+                    break;
+                default:
+                    break; // Shouldn't get here.                
+            }
+            
+        }
+        
+    }
+
+
+    // TODO Kommentera bättre
+    
 
     class Program
     {        
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Welcome to my minified database of beverages!\nSelect category item with a number.\n");
+            Console.WriteLine("1 - Beer\n2 - Wine\n3 - Whisky\n9 - List all your added things\n0 - Exit");
+        }
         static void Main(string[] args)
         {
             DbMiddleware middleware = new DbMiddleware();
 
-            Console.WriteLine("Welcome to my minified database of beverages!\nSelect category item with a number.\n");
-            Console.WriteLine("1 - Beer\n2 - Wine\n3 - Whisky\n9 - List all your added things\n0 - Exit");
-            string s = Console.ReadLine();
-
-            while (!middleware.checkCategory(s))
+            string s = "";
+            while (!s.Equals("0"))
             {
-                Console.WriteLine("Bad input, try again.");
+                PrintMenu();
                 s = Console.ReadLine();
-            }
 
-            Console.WriteLine("Chosen category: {0}",middleware.category);
+                while (!middleware.checkCategory(s))
+                {
+                    Console.WriteLine("Bad input, try again.");
+                    s = Console.ReadLine();
+                }
 
-            
+//                Console.WriteLine("Chosen category: {0}", middleware.category);
+
+
+            }           
+           
         }
     }
 }
